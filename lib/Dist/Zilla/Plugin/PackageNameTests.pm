31b456c2c40dde10fba418f549b58043fe757e87
Ray Miller <rm7@sanger.ac.uk>
2012-04-24 12:33:07 +0100

    Initial check-in

---
+++ lib/Dist/Zilla/Plugin/PackageNameTests.pm
@@ -0,0 +1,146 @@
+package Dist::Zilla::Plugin::PackageNameTests;
+
+use strict;
+use warnings;
+
+# ABSTRACT: tests to check that the package name matches the file name of your modules
+
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+with    'Dist::Zilla::Role::FileMunger';
+
+
+# -- attributes
+
+# skiplist - a regex
+has skip => ( is=>'ro', predicate=>'has_skip' );
+
+
+# -- public methods
+
+# called by the filemunger role
+sub munge_file {
+    my ($self, $file) = @_;
+
+    return unless $file->name eq 't/00-package-names.t';
+
+    my $replacement = ( $self->has_skip && $self->skip )
+        ? sprintf( 'return if $found =~ /%s/;', $self->skip )
+        : '# nothing to skip';
+
+    # replace the string in the file
+    my $content = $file->content;
+    $content =~ s/PACKAGENAMETESTS_SKIP/$replacement/;
+    $file->content( $content );
+}
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::PackageNameTests - tests to check that the package name matches the file name of your modules
+
+=begin Pod::Coverage
+
+munge_file
+
+=end Pod::Coverage
+
+=head1 SYNOPSIS
+
+In your dist.ini:
+
+    [PackageNameTests]
+    skip = Test$
+
+=head1 DESCRIPTION
+
+This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing
+the following files:
+
+=over 4
+
+=item * t/00-package-names.t - a standard test to check the package declaration
+
+This test will find all modules in your dist, and check that the package name
+is consistent with the file name, e.g. a module found in C<lib/My/Module.pm> should
+contain a declaration C<package My::Module>.
+
+=back 
+
+This plugin accepts the following options:
+
+=over 4
+
+=item * skip: a regex to skip the package test for modules matching it. The
+match is done against the module name (C<Foo::Bar>), not the file path
+(F<lib/Foo/Bar.pm>).
+
+=back 
+
+=head1 COPYRIGHT AND LICENSE
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut 
+
+
+
+__DATA__
+___[ t/00-package-names.t ]___
+#!perl
+
+use strict;
+use warnings;
+
+use Test::Most 'bail';
+use File::Find;
+
+sub expected_package_name($) {
+  my $file = shift;
+  $file =~ s{^.*lib/}{};
+  $file =~ s{\.pm$}{};
+  $file =~ s/\//::/g;
+  return $file;
+}
+
+sub found_package_name($) {
+  my $file = shift;
+
+  # we assume first package name found is actual
+  open my $fh, '<', $file or die "Could not open $file for reading: $!";
+  my $package;
+  while ( my $line = <$fh> ) {
+    next unless $line =~ /^\s*package\s+((?:\w+)(::\w+)*)/;
+    return $1;
+  }
+}
+
+my @files;
+find(
+  sub {
+    my $found = $File::Find::name;
+    return unless $found =~ /\.pm\z/ and -f $found;
+    PACKAGENAMETESTS_SKIP
+    push @files, [ $found, found_package_name $found, expected_package_name $found ];
+  },
+  'lib',
+);
+
+plan tests => scalar( @files ) || 1;
+
+if ( @files ) {
+    for my $file ( @files ) {
+        my ( $file, $have, $want ) = @$file;
+        is $have, $want, "Package name correct for $file";
+    }
+}
+else {
+    ok 1, 'no modules to test';
+}
---
+++ lib/Dist/Zilla/PluginBundle/Author/HTGT.pm
@@ -0,0 +1,170 @@
+package Dist::Zilla::PluginBundle::Author::HTGT;
+
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+# skip these dependencies
+has skip_deps => (
+    is  => 'ro',
+    isa => 'Maybe[Str]',
+    lazy => 1,
+    default => sub { $_[0]->payload->{skip_deps} || '' },
+);
+
+# skip these files
+has skip_files => (
+    is  => 'ro',
+    isa => 'Maybe[Str]',
+    lazy => 1,
+    default => sub { $_[0]->payload->{skip_files} || '' },
+);
+
+sub configure {
+    my $self = shift;
+
+    $self->add_bundle(
+        '@Filter' => {
+            '-bundle' => '@Basic',
+            '-remove' => [ 'UpleadToCPAN' ],
+        }
+    );
+
+    $self->add_plugins(
+        [
+            'AutoPrereqs' => {
+                length $self->skip_deps ? ( 'skip' => [ $self->skip_deps ] ) : ()
+            }
+        ],
+        'Test::Compile',
+        'NoSmartCommentsTests',
+        'PackageNamesTests',
+        'PkgVersion',
+        'NextRelease',
+        'FakeRelease'
+        [
+            'PruneFiles' => {
+                'filenames' => 'dist.ini',
+                length $self->skip_files ? ( 'match' => [ $self->skip_files ] ) : ()
+            }
+        ],
+        'Git::NextVersion',        
+        [
+            'Git::CommitBuild' => {
+                branch          => '',                
+                release_branch  => 'releases',
+                release_message => ( $self->_get_changes || 'Build results of %h on %b' )
+            }
+        ],
+        # CommitBuild -must- come before these
+        'Git::Check',
+        'Git::commit',
+        [
+            'Git::Tag' => {
+                branch = 'releases'
+            }
+        ],
+        'Git::Push',
+    );
+    $self->add_bundle('@Git' => {
+        'commit_msg' => 'Bumped changelog following rel. v%v'
+    });
+}
+
+# stolen from Dist::Zilla::Plugin::Git::Commit
+sub _get_changes {
+    my $self = shift;
+
+    # parse changelog to find commit message
+    my $changelog = Dist::Zilla::File::OnDisk->new( { name => 'Changes' } );
+    my $newver    = '{{\$NEXT}}';
+    my @content   =
+        grep { /^$newver(?:\s+|$)/ ... /^\S/ } # from newver to un-indented
+        split /\n/, $changelog->content;
+    shift @content; # drop the version line
+    # drop unindented last line and trailing blank lines
+    pop @content while ( @content && $content[-1] =~ /^(?:\S|\s*$)/ );
+
+    # return commit message
+    return join("\n", @content, ''); # add a final \n
+} # end _get_changes
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+
+=pod
+
+=head1 DESCRIPTION
+
+This is the plugin bundle that HTGT uses. It is equivalent to:
+
+ [@Filter]
+ -bundle = @Basic
+ -remove = Readme
+
+ [AutoPrereqs]
+
+ [Test::Compile]
+
+ [NoSmartCommentsTests]
+
+ [PackageNamesTests]
+
+ [PkgVersion]
+
+ [NextRelease]
+
+ [FakeRelease]
+
+ [PruneFiles]
+ filename = dist.ini
+
+ [Git::NextVersion]
+
+ [Git::CommitBuild]
+ branch          =
+ release_branch  = releases
+ release_message = <changelog section content>
+
+ [Git::Check]
+
+ [Git::Commit]
+
+ [Git::Tag]
+
+ [Git::Push]
+
+=head1 CONFIGURATION
+
+If you provide a value to the C<skip_deps> option then it will be passed to
+the C<AutoPrereqs> Plugin as the C<skip> attribute.
+
+If you provide a value to the C<skip_files> option then it will be passed to
+the C<PruneFiles> Plugin as the C<match> attribute.
+
+=head1 TIPS
+
+Do not include a C<NAME>, C<VERSION>, C<AUTHOR> or C<LICENSE> POD section in
+your code, they will be provided automatically.
+
+The bundle is desgined for projects which are hosted on C<github>.
+More so, the project should have a C<master> branch where you do code
+development, and a separete 'releases' branch which is where the
+I<built> code is committed.
+
+=head1 AUTHOR
+
+Ray Miller <raym@cpan.org>, based on code and ideas from Dist::Zilla::PluginBundle::Author::OLIVER.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Ray Miller.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+__END__
+
